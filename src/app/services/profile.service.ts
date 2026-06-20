ee3b723298d317716bcd045970a5165863cdcb3e
arun76o <arunsai2029@gmail.com>
2026-06-20 15:21:22 +0530

    Connect TradeService to Render API

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserProfile } from '../models/trade.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  private apiUrl = 'https://localhost:7265/api/Profile';
+  private apiUrl = `${environment.apiUrl}/Trades`;
 
   constructor(private http: HttpClient) { }
 
